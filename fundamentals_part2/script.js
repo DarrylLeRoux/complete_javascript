9cc1022d68825af79e82af3c3d9d8afffc7bd81d
Darryl Le Roux <devdarryl@outlook.com>
2023-02-07 22:24:13 +0200

    Wayside files

--- fundamentals_part2/script.js
+++ fundamentals_part2/script.js
@@ -10,10 +10,12 @@ function logger() {
 logger();
 
 function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
   const juice = `Juice with ${apples} apples, and ${oranges} oranges.`;
   return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice); // or console.log(fruitProcessor(5, 0))
+
+const appleOrangeJuice = fruitProcessor(2, 3);
+console.log(appleOrangeJuice);

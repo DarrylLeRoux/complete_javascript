33b92c2008c933eac0709fd67b971c43ec51a56f
Darryl Le Roux <devdarryl@outlook.com>
2023-02-07 22:30:37 +0200

    Declarations vs expressions

--- fundamentals_part2/script.js
+++ fundamentals_part2/script.js
@@ -19,3 +19,19 @@ console.log(appleJuice); // or console.log(fruitProcessor(5, 0))
 
 const appleOrangeJuice = fruitProcessor(2, 3);
 console.log(appleOrangeJuice);
+
+// Declaration vs Expression
+
+// Function Declaration
+function calcAge1(birthYear) {
+  const age = 2023 - birthYear;
+  return age;
+}
+console.log(calcAge1(1982));
+
+// Function Expression
+const calAge2 = function (birthYear) {
+  return 2023 - birthYear;
+};
+const age2 = calAge2(1982);
+console.log(age2);

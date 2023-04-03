35681db0ba78b20a70266c8d80ebc3cb9c34eebe
Darryl Le Roux <devdarryl@outlook.com>
2023-04-03 22:00:00 +0200

    Added data types

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+let firstName = 'Jonas';
+console.log(firstName);
+
+// Values and Variables
+let country = 'South Africa';
+let continent = 'Africa';
+let population = 36_000_000;
+console.log(country, continent, population);
+
+// Data Types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// let, const and var
+language = 'English';
+// const country = 'South Africa'; // error
+// const continent = 'Africa'; // error
